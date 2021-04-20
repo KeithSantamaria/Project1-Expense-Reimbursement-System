7f648477109681f532861974547278ddd3018e50
August Duet <august.duet@revature.com>
2021-04-20 16:21:43 -0400

    added log4j config example

--- Week1_Java/intro-to-gradle/src/main/java/com/ex/Main.java
+++ Week1_Java/intro-to-gradle/src/main/java/com/ex/Main.java
@@ -1,7 +1,12 @@
 package com.ex;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Main {
+    public static final Logger logger = LogManager.getLogger(Main.class.getName());
+    public static final Logger rootLogger = LogManager.getRootLogger();
+    public static void main(String[] args) {
+        logger.debug("Hello, DEBUG");
     }
 }
---
+++ Week1_Java/intro-to-gradle/src/test/java/com/ex/Tests.java
@@ -0,0 +1,12 @@
+package com.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tests {
+    @Test
+    public void shouldPass() {
+        assertTrue(true);
+    }
+}
